864672d68e16c1fbfcbae6e79d75ebbcf90dab76
Samuel Marques <snamuelfilho2011@gmail.com>
2023-03-12 20:20:12 -0300

    Criação de Logs para a Aplicação

--- osworks/src/main/java/com/albino/tecnologia/osworks/service/impl/ContratoServiceImpl.java
+++ osworks/src/main/java/com/albino/tecnologia/osworks/service/impl/ContratoServiceImpl.java
@@ -5,18 +5,24 @@ import com.albino.tecnologia.osworks.model.Contrato;
 import com.albino.tecnologia.osworks.repository.ContratoRepository;
 import com.albino.tecnologia.osworks.service.ContratoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class ContratoServiceImpl implements ContratoService {
     private final ContratoRepository contratoRepository;
 
     @Override
     public Contrato encontrarPeloIdContrato(Long id) {
+
+        log.info("Encontrado O Contrato com '{}'", id);
+
         return contratoRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Id Não Encontrado"));
+                .orElseThrow(() -> new RuntimeException("Id Não Encontrado"));
     }
 
     @Override
@@ -26,6 +32,8 @@ public class ContratoServiceImpl implements ContratoService {
 
     @Override
     public Contrato criarContrato(ContratoDTO contratoDTO) {
+
+        log.info("Novo Contrado Criado '{}'", contratoDTO.toString());
 
         Contrato novoContrato = Contrato.builder()
                 .numero(contratoDTO.getNumero())
@@ -40,8 +48,11 @@ public class ContratoServiceImpl implements ContratoService {
     }
 
     @Override
-    public Contrato atualizarContrato(Long id,ContratoDTO contratoDTO) {
+    public Contrato atualizarContrato(Long id, ContratoDTO contratoDTO) {
+
         Contrato contratoAtualizado = encontrarPeloIdContrato(id);
+
+        log.info(" Contrado Com Id: '{}' Sendo Atualizado",id);
 
         contratoAtualizado.setDataInicio(contratoDTO.getDataInicio());
         contratoAtualizado.setDataTermino(contratoDTO.getDataTermino());
@@ -49,13 +60,20 @@ public class ContratoServiceImpl implements ContratoService {
         contratoAtualizado.setDescricao(contratoDTO.getDescricao());
         contratoAtualizado.setProdutoOuServico(contratoDTO.getProdutoOuServico());
 
+        log.info(" Contrado Com Id: '{}' Foi Atualizado Para '{}' ",id,contratoAtualizado.toString());
+
         return contratoRepository.save(contratoAtualizado);
     }
 
     @Override
     public void deletarContrato(Long id) {
+
+        log.info(" Contrado Com Id: '{}' Sendo Inativado",id);
+
         Contrato contratoDeletado = encontrarPeloIdContrato(id);
 
         contratoRepository.delete(contratoDeletado);
+
+        log.info(" Contrado Com Id: '{}' Foi Inativado",id);
     }
 }
