899e00334ec232e65aede83a648a85550fe51f49
Samuel Marques <snamuelfilho2011@gmail.com>
2023-03-24 18:48:12 -0300

    Atualização nas configurações de Segurança

--- osworks/src/main/java/com/albino/tecnologia/osworks/config/WebSecurityConfig.java
+++ osworks/src/main/java/com/albino/tecnologia/osworks/config/WebSecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
@@ -33,7 +38,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-       http.cors().and().csrf().disable();
+        http.cors().and().csrf().disable();
 
         http
 
@@ -52,7 +57,15 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    CorsConfigurationSource corsConfigurationSource() {
 
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
